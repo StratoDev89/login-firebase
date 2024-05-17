a1c1741f9e2f4d08aa6f3821548571a38d0f8734
StratoDev89 <danieltestingemail01@gmail.com>
2024-05-17 06:20:18 -0400

    first commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,5 @@
 export const environment = {
    firebaseConfig : {
-    apiKey: "AIzaSyA17VtYnbIuy-zE59FGW1Xs6Z2_w5rIwEU",
-    authDomain: "login-gpt-a690c.firebaseapp.com",
-    projectId: "login-gpt-a690c",
-    storageBucket: "login-gpt-a690c.appspot.com",
-    messagingSenderId: "210853376546",
-    appId: "1:210853376546:web:6554c7f48afc5b60c1d07b",
-    measurementId: "G-72J835KGGS"
+
   }
 };
